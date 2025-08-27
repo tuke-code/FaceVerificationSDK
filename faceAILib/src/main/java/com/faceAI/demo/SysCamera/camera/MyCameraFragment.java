3e67cb61dc5cf78c32aabbeb7b2a875f40758929
FaceSDK <FaceAISDK.Service@gmail.com>
2025-08-27 09:49:55 +0800

    完善自定义摄像头 管理说明

--- faceAILib/src/main/java/com/faceAI/demo/SysCamera/camera/MyCameraFragment.java
+++ faceAILib/src/main/java/com/faceAI/demo/SysCamera/camera/MyCameraFragment.java
@@ -31,7 +31,6 @@ import java.util.concurrent.Executors;
  * 你也可以使用老的Camera2 相机等方式管理摄像头，通过预览流回调数据转为Bitmap 后持续送入SDK
  * FaceSearchEngine.Companion.getInstance().runSearchWithBitmap(bitmap); //不要在主线程调用
  *
- *
  * @author FaceAISDK.Service@gmail.com
  */
 public class MyCameraFragment extends Fragment implements CameraXConfig.Provider{
