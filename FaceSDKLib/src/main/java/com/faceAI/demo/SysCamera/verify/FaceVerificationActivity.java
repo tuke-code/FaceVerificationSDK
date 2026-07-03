1f8c49dbcb15e7266ebbbeed646eb21b4633ebd7
FaceSDK <FaceAISDK.Service@gmail.com>
2026-07-03 11:30:55 +0800

    添加英文支持，优化细节

--- FaceSDKLib/src/main/java/com/faceAI/demo/SysCamera/verify/FaceVerificationActivity.java
+++ FaceSDKLib/src/main/java/com/faceAI/demo/SysCamera/verify/FaceVerificationActivity.java
@@ -145,9 +145,9 @@ public class FaceVerificationActivity extends AbsBaseActivity {
                 .setCameraType(FaceAICameraType.SYSTEM_CAMERA)  //相机类型，目前分为3种
                 .setCompareDurationTime(3000)           //人脸识别超时时间[3000,6000] 毫秒
                 .setLivenessType(faceLivenessType)      //活体检测类型。
-                .setMotionLivenessStepSize(motionStepSize)    //随机动作活体的步骤个数[1-2]，SILENT_MOTION和MOTION 才有效
-                .setMotionLivenessTimeOut(motionTimeOut)      //动作活体检测，支持设置超时时间 [3,22] 秒 。API 名字0410 修改
                 .setMotionLivenessTypes(motionLivenessTypes)  //动作活体种类。1 张张嘴,2 微笑,3 眨眨眼,4 摇摇头,5 点点头
+                .setMotionLivenessStepSize(motionStepSize)   //从「动作活体种类」随机动作活体的步骤个数[1-2]，SILENT_MOTION和MOTION 才有效
+                .setMotionLivenessTimeOut(motionTimeOut)     //动作活体检测，支持设置超时时间 [3,22] 秒 。API 名字0410 修改
                 .setStopVerifyNoFaceRealTime(false)      //没检测到人脸是否立即停止，还是出现过人脸后检测到无人脸停止.(默认false，为后者)
                 .setProcessCallBack(new ProcessCallBack() {
                     /**
@@ -215,7 +215,7 @@ public class FaceVerificationActivity extends AbsBaseActivity {
 
         if (isVerifyMatched&&(livenessValue>0.8||faceLivenessType.equals(FaceLivenessType.NONE))) {
             //2. 相似度>verifyThreshold，并且livenessValue>0.8
-            TTSPlayer.getInstance().playTTS(R.string.face_verify_success);
+            //TTSPlayer.getInstance().playTTS(R.string.face_verify_success);
             new ImageToast().show(getApplicationContext(), getString(R.string.face_verify_success));
             new Handler(Looper.getMainLooper()).postDelayed(() -> {
                 finishFaceVerify(VERIFY_SUCCESS, R.string.face_verify_result_success, similarity,livenessValue);
@@ -227,13 +227,13 @@ public class FaceVerificationActivity extends AbsBaseActivity {
             new AlertDialog.Builder(FaceVerificationActivity.this)
                     .setMessage(R.string.face_verify_result_failed)
                     .setCancelable(false)
-                    .setPositiveButton(R.string.retry, (dialogInterface, i) -> {
-                        retryTime++;
-                        if (retryTime > 2) {
+                    .setPositiveButton(retryTime > 3 ? R.string.confirm : R.string.retry, (dialogInterface, i) -> {
+                        if (retryTime > 3) {
                             finishFaceVerify(code, R.string.face_verify_result_failed, similarity,livenessValue);
                         } else {
                             faceVerifyUtils.retryVerify();
                         }
+                        retryTime++;
                     }).show();
         }
     }
@@ -267,13 +267,13 @@ public class FaceVerificationActivity extends AbsBaseActivity {
                     new AlertDialog.Builder(this)
                             .setMessage(R.string.motion_liveness_detection_time_out)
                             .setCancelable(false)
-                            .setPositiveButton(R.string.retry, (dialogInterface, i) -> {
-                                retryTime++;
-                                if (retryTime > 1) {
+                            .setPositiveButton(retryTime > 2 ? R.string.confirm : R.string.retry, (dialogInterface, i) -> {
+                                if (retryTime > 2) {
                                     finishFaceVerify(MOTION_LIVENESS_TIMEOUT, R.string.face_verify_result_timeout);
                                 } else {
                                     faceVerifyUtils.retryVerify();
                                 }
+                                retryTime++;
                             }).show();
                     break;
 
@@ -321,10 +321,15 @@ public class FaceVerificationActivity extends AbsBaseActivity {
                 case NO_FACE_REPEATEDLY:
                     setMainTips(R.string.no_face_or_repeat_switch_screen);
                     new AlertDialog.Builder(this)
-                            .setMessage(R.string.stop_verify_tips)
+                            .setMessage(R.string.no_face_repeatedly)
                             .setCancelable(false)
-                            .setPositiveButton(R.string.confirm, (dialogInterface, i) -> {
-                                finishFaceVerify(NO_FACE_MULTI, R.string.face_verify_result_no_face_multi_time);
+                            .setPositiveButton(retryTime > 2 ? R.string.confirm : R.string.retry, (dialogInterface, i) -> {
+                                if (retryTime > 2) {
+                                    finishFaceVerify(NO_FACE_MULTI, R.string.face_verify_result_no_face_multi_time);
+                                } else {
+                                    faceVerifyUtils.retryVerify();
+                                }
+                                retryTime++;
                             }).show();
                     break;
 
@@ -363,13 +368,13 @@ public class FaceVerificationActivity extends AbsBaseActivity {
                     new AlertDialog.Builder(this)
                             .setMessage(R.string.color_flash_liveness_failed)
                             .setCancelable(false)
-                            .setPositiveButton(R.string.retry, (dialogInterface, i) -> {
-                                retryTime++;
-                                if (retryTime > 1) {
+                            .setPositiveButton(retryTime > 2 ? R.string.confirm : R.string.retry, (dialogInterface, i) -> {
+                                if (retryTime > 2) {
                                     finishFaceVerify(COLOR_LIVENESS_FAILED, R.string.color_flash_liveness_failed);
                                 } else {
                                     faceVerifyUtils.retryVerify();
                                 }
+                                retryTime++;
                             }).show();
                     break;
 
@@ -379,13 +384,13 @@ public class FaceVerificationActivity extends AbsBaseActivity {
                     new AlertDialog.Builder(this)
                             .setView(dialogView) // 【关键】设置自定义的 View
                             .setCancelable(false)
-                            .setPositiveButton(R.string.retry, (dialogInterface, i) -> {
-                                retryTime++;
-                                if (retryTime > 1) {
+                            .setPositiveButton(retryTime > 2 ? R.string.confirm : R.string.retry, (dialogInterface, i) -> {
+                                if (retryTime > 2) {
                                     finishFaceVerify(COLOR_LIVENESS_LIGHT_TOO_HIGH, R.string.color_flash_light_high);
                                 } else {
                                     faceVerifyUtils.retryVerify();
                                 }
+                                retryTime++;
                             }).show();
                     break;
 
